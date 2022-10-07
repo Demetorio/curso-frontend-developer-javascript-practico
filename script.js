f5544c6db53b2d4247a9882c177d0c3c9b0aeacd
Vicente <valderramavicente914@gmail.com>
2022-10-07 00:27:06 -0300

    fusionando el navbar y el mobile menu en la version de mobile

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const stripesMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
 
 function toogleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+stripesMenu.addEventListener('click', toogleMobileMenu)
+
+function toogleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }

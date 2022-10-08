fc5309d0bee3b42cb4495dfa34b7f60f4d6b7e3e
Vicente <valderramavicente914@gmail.com>
2022-10-08 00:42:51 -0300

    añadido aside de shopping cart para mobile y desktop

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const stripesMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCartMenu = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
 
@@ -12,5 +14,19 @@ function toogleDesktopMenu() {
 stripesMenu.addEventListener('click', toogleMobileMenu)
 
 function toogleMobileMenu() {
+    const asideClosed = aside.classList.contains('inactive');
+    if (!asideClosed) {
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+
+shoppingCartMenu.addEventListener('click', toogleAside)
+
+function toogleAside() {
+    const mobileMenuClosed = mobileMenu.classList.contains('inactive');
+    if (!mobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
 }
